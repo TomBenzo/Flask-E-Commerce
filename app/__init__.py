c18ec4f1283a8af25443d0324684783fd1c79d9e
TomBenzo <98659510+TomBenzo@users.noreply.github.com>
2022-03-21 14:44:35 -0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from config import Config
+
+from .auth.routes import auth
+from .shop.routes import shop
+
+from .models import Buyer, db
+from flask_migrate import Migrate
+
+from flask_login import LoginManager
+
+app = Flask(__name__)
+login = LoginManager()
+
+@login.user_loader
+def load_user(user_id):
+    return Buyer.query.get(user_id)
+
+app.register_blueprint(auth)
+app.register_blueprint(shop)
+
+app.config.from_object(Config)
+
+db.init_app(app)
+login.init_app(app)
+
+migrate = Migrate(app,db)
+
+from . import routes
+from . import models
+
